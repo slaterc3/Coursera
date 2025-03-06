0c1580fa795eb73812896f91d027162c7d527c2b
Christopher Guy Slater <cslat22@wgu.edu>
2025-03-06 07:35:06 -0500

    created df, csv and .db files

--- ibm/dataEngineeringProfCert/PythonProject/web_scraping.py
+++ ibm/dataEngineeringProfCert/PythonProject/web_scraping.py
@@ -7,7 +7,7 @@ url = 'https://web.archive.org/web/20230902185655/https://en.everybodywiki.com/1
 
 db_name = 'Movies.db'
 table_name = 'Top_50'
-csv_path = '/home/project/top_50_films.csv'
+csv_path = 'C:\\Users\\slate\\Coursera\\ibm\\dataEngineeringProfCert\\PythonProject\\top_50_films.csv'
 df = pd.DataFrame(columns=['Average Rank','Film', 'Year'])
 count = 0
 
@@ -15,4 +15,26 @@ html_page = requests.get(url).text # .text solved issue (BS expects string)
 data = BeautifulSoup(html_page, 'html.parser')
 
 tables = data.find_all('tbody')
-print(tables)
+# print(tables)
+rows = tables[0].find_all('tr')
+
+# print(rows)
+for idx, row in enumerate(rows):
+    data_dict = {}
+    if idx < 50:
+        col = row.find_all('td')
+        # print(col)
+        if len(col) != 0: # if columns exist
+            data_dict['Average Rank'] = col[0].contents[0]
+            data_dict['Film'] = col[1].contents[0]
+            data_dict['Year'] = col[2].contents[0]
+            df1 = pd.DataFrame(data_dict, index=[0])
+            df = pd.concat([df,df1], ignore_index=True)
+    else:
+        break 
+
+df.to_csv(csv_path)
+
+conn = sqlite3.connect(db_name)
+df.to_sql(table_name, conn, if_exists='replace', index=False)
+conn.close()
